68a37f4df4be159a946f7464c8b6c3c7d53a03ed
John Carlson <john.carlson3@sbcglobal.net>
2014-12-15 15:10:23 -0600

    fiddled a bit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+var http = require('http');
 
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
 app.use(express.static(path.join(__dirname, 'public')));
 
-var port = 8080;
-app.listen(port);
-console.log('http server started on port %s', port);
+http.createServer(app).listen(app.get('port'), function () {
+	console.log('express server started on port %s', app.get('port'));
+});
