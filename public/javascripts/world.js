0443385e96c97359c35fde53d3a6751b31db261c
John Carlson <john.carlson3@sbcglobal.net>
2014-12-19 18:10:25 -0600

    added usa and world maps.  Worked on getting sentiment analysis and worldmap together

---
+++ public/javascripts/world.js
@@ -0,0 +1,35 @@
+var width = 960,
+    height = 960;
+
+var projection = d3.geo.mercator()
+    .scale((width + 1) / 2 / Math.PI)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var graticule = d3.geo.graticule();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+d3.json("world-50m.json", function(error, world) {
+  svg.insert("path", ".graticule")
+      .datum(topojson.feature(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+});
+
+d3.select(self.frameElement).style("height", height + "px");
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,24 @@ exports.index = function(req, res){
   res.render('index', { title: "Twit-Decision"});
 };
 
+exports.map = function(req, res){
+  res.render('map', { title: "Twit-Map"});
+};
+
 exports.ping = function(req, res){
   res.send("pong!", 200);
 };
 
-function performAnalysis(tweetSet) {
+function performAnalysis(tweetSet, geoSet) {
   //set a results variable
   var results = 0;
   // iterate through the tweets, pulling the text, retweet count, and favorite count
   for(var i = 0; i < tweetSet.length; i++) {
     tweet = tweetSet[i]['text'];
+    if (tweetSet[i].geo != null) {
+	tweetSet[i].geo['tweet'] = tweet;
+	geoSet.push(tweetSet[i].geo);
+    }
     retweets = tweetSet[i]['retweet_count'];
     favorites = tweetSet[i]['favorite_count'];
     // remove the hastag from the tweet text
@@ -68,22 +76,24 @@ exports.search = function(req, res) {
   var highestChoice = null;
   // create new array
   var array = [];
+  var geoSet = [];
   // set score
   var score = 0;
   console.log("----------")
 
-  // iterate through the choices array from the request
+// iterate through the choices array from the request
   for(var i = 0; i < choices.length; i++) {
     (function(i) {
     // add choice to new array
     array.push(choices[i])
     // grad 20 tweets from today
     twitter.get('search/tweets', {q: '' + choices[i] + ' since:' + today.getFullYear() + '-' +
-      (today.getMonth() + 1) + '-' + today.getDate(), count:20}, function(err, data) {
+      (today.getMonth() + 1) + '-' + today.getDate(), count:2000}, function(err, data) {
         // perform sentiment analysis
-        score = performAnalysis(data['statuses']);
+	score = performAnalysis(data['statuses'], geoSet);
         console.log("score:", score)
         console.log("choice:", choices[i])
+        console.log("geoSet:", geoSet)
         //  determine winner
         if(score > highestScore) {
           highestScore = score;
@@ -95,5 +105,5 @@ exports.search = function(req, res) {
     })(i)
   }
   // send response back to the server side; why the need for the timeout?
-  setTimeout(function() { res.end(JSON.stringify({'score': highestScore, 'choice': highestChoice})) }, 5000);  
+  setTimeout(function() { res.end(JSON.stringify({'score': highestScore, 'choice': highestChoice, 'geoSet': geoSet })) }, 5000);  
 };
